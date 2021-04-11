d0a612c3b94d434a1667fff1561eb75acdfeaa98
lihongw <2291781928@qq.com>
2021-04-11 17:05:06 +0800

    去掉查询方法的事务

--- imooc-homepage-service/homepage-course/src/main/java/com/imooc/homepage/service/impl/CourseServiceImpl.java
+++ imooc-homepage-service/homepage-course/src/main/java/com/imooc/homepage/service/impl/CourseServiceImpl.java
@@ -8,7 +8,6 @@ import com.imooc.homepage.service.ICourseService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import java.util.Collections;
@@ -49,7 +48,6 @@ public class CourseServiceImpl implements ICourseService {
     }
 
 
-    @Transactional
     @Override
     public List<CourseInfo> getCourseInfos(CourseInfoRequest request) {
         if(CollectionUtils.isEmpty(request.getIds())){
